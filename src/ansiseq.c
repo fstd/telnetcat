50ae72459c597ae35d698ebfc94e4502b7286bc1
fstd <van.fstd@gmail.com>
2015-12-21 01:45:21 +0100

    improve ansi sequence state machine

--- src/ansiseq.c
+++ src/ansiseq.c
@@ -13,46 +13,65 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 #include "intlog.h"
 #include "common.h"
 
-#define ST_ST 0 /* Starting state */
-#define ST_E1 1 /* escape char seen */
-#define ST_CSI 2 /* CSI state */
-#define ST_PAC 3 /* ) state */
-#define ST_PAO 4 /* ( state */
-#define ST_SQC 5 /* ] state */
-#define ST_ERR 6 /* error state (accepting) */
-#define ST_FIN 7 /* okay state (accepting) */
-#define ST_IGN 8 /* ignore state (accepting) */
+#define S_STA 0 /* Starting state */
+#define S_ESC 1 /* escape char seen */
+#define S_CSI 2 /* CSI state */
+#define S_SQC 3 /* ] state */
+#define S_PPH 4 /* ()# state */
+#define S_FIN 5 /* okay state (accepting) */
+#define S_ERR 6 /* error state (accepting) */
+#define NUM_STATES 7
 
-#define TK_ESC   0 /* Escape character */
-#define TK_SQO 1 /* [ */
-#define TK_SQC 2 /* ] */
-#define TK_PAC 3 /* ) */
-#define TK_PAO 4 /* ( */
-#define TK_QM 5 /* ? */
-#define TK_SC 6 /* ; */
-#define TK_ALPHA 7 /* a-zA-Z */
-#define TK_DIGIT 8 /* 0-9 */
-#define TK_CH    9 /* Anything else */
+/* digits can be command chars, too, but not for CSI */
+#define CCHRS "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz<=>"
+#define T_ESCP 0 /* esc ('\033') */
+#define T_CCHR 1 /* see CCHRS */
+#define T_NDIG 2 /* '0' - '9' */
+#define T_BELL 3 /* '\a' */
+#define T_STRM 4 /* '\234' */
+#define T_HASH 5 /* '#' */
+#define T_OPRN 6 /* '(' */
+#define T_CPRN 7 /* ')' */
+#define T_OSQB 8 /* '[' */
+#define T_CSQB 9 /* ']' */
+#define T_QMRK 10 /* '?' */
+#define T_SMCL 11 /* ';' */
+#define T_REST 12 /* anything else */
+#define NUM_TOKENS 13
+
+#define MAX_CALLBACKS 64
 
 struct transition {
 	int state;
 	void (*action)(int);
 };
 
-static int s_parmv = 0;
-static struct ansiseq s_curseq;
-static struct ansiseq s_protoseq;
-static bool s_gotparm;
-static bool s_valid;
-
 static bool s_init = false;
+static struct ansiseq s_protoseq;
+
+static struct ansiseq s_curseq;
+
+static int s_parmv = 0;
+static bool s_gotparm;
+
+struct ansiseq_match {
+	uint8_t cls;
+	uint8_t cmd;
+	ansiseq_cb_fn fn;
+};
+
+static struct ansiseq_match s_cbs[MAX_CALLBACKS];
+static ansiseq_cb_fn s_cbunhandled;
+
 
 static void ansiseq_init(void);
+
 
 void
 act_nop(int c)
@@ -65,7 +84,6 @@ act_begin(int c)
 	s_curseq = s_protoseq;
 	s_gotparm = false;
 	s_parmv = 0;
-	s_valid = false;
 }
 
 void
@@ -87,7 +105,6 @@ act_mkctl(int c)
 		act_stor(0);
 	
 	s_curseq.cmd = c;
-	s_valid = true;
 }
 
 void
@@ -98,69 +115,71 @@ act_mkpar(int c)
 }
 
 void
+act_mkstr(int c)
+{
+	size_t len = strlen((const char *)s_curseq.strarg);
+	if (len + 1 >= sizeof s_curseq.strarg) {
+		W("OSC control sequence truncated");
+		return;
+	}
+
+	s_curseq.strarg[len] = c;
+}
+
+void
+act_setcls(int c)
+{
+	s_curseq.cls = c;
+}
+
+void
 act_ext(int c)
 {
-	s_curseq.ext = false;
+	s_curseq.ext = true;
 }
 
 int
 mktok(int c)
 {
-	if (isdigit(c))
-		return TK_DIGIT;
+	if (c < 0 || c > 255)
+		C("character out of range: %d", c);
 
-	if (isalpha(c))
-		return TK_ALPHA;
+	if (c == 0)           return T_REST;
+	if (isdigit(c))       return T_NDIG;
+	if (strchr(CCHRS, c)) return T_CCHR;
 
 	switch (c) {
-	case '\033': return TK_ESC;
-	case '[':    return TK_SQO;
-	case ']':    return TK_SQC;
-	case ')':    return TK_PAC;
-	case '(':    return TK_PAO;
-	case '?':    return TK_QM;
-	case ';':    return TK_SC;
-	default:     return TK_CH;
+	case '\033': return T_ESCP;
+	case '\007': return T_BELL;
+	case '\234': return T_STRM;
+	case '#':    return T_HASH;
+	case '(':    return T_OPRN;
+	case ')':    return T_CPRN;
+	case '[':    return T_OSQB;
+	case ']':    return T_CSQB;
+	case '?':    return T_QMRK;
+	case ';':    return T_SMCL;
+	default:     return T_REST;
 	}
 }
 
-struct transition delta[9][10] = {
-/*          TK_ESC              TK_SQO             TK_SQC                TK_PAC               TK_PAO              */
-/*            TK_QM               TK_SC               TK_ALPHA             TK_DIGIT             TK_CH             */
-/*ST_ST */ {{ST_E1, act_begin}, {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_E1 */ {{ST_ERR, act_nop},  {ST_CSI, act_nop},  {ST_SQC, act_nop},   {ST_PAC, act_nop},   {ST_PAO, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_IGN, act_nop},   {ST_IGN, act_nop}},
-
-/*ST_CSI*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_CSI, act_ext},  {ST_CSI, act_stor}, {ST_FIN, act_mkctl}, {ST_CSI, act_mkpar}, {ST_FIN, act_mkctl}},
-
-/*ST_PAC*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_IGN, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_PAO*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_IGN, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_SQC*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_IGN, act_nop},  {ST_ERR, act_nop},   {ST_SQC, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_ERR*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_FIN*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop}},
-
-/*ST_IGN*/ {{ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop},
-              {ST_ERR, act_nop},  {ST_ERR, act_nop},  {ST_ERR, act_nop},   {ST_ERR, act_nop},   {ST_ERR, act_nop}},
+#define ERR {S_ERR, act_nop}
+struct transition delta[][NUM_TOKENS] = {
+/*           T_ESCP              T_CCHR              T_NDIG              T_BELL            T_STRM            T_HASH               T_OPRN               T_CPRN               T_OSQ                T_CSQB               T_QMRK              T_SMCL              T_REST */
+/* S_STA */ {{S_ESC, act_begin}, ERR,                ERR,                ERR,              ERR,              ERR,                 ERR,                 ERR,                 ERR,                 ERR,                 ERR,                ERR,                ERR,                },
+/* S_ESC */ {{S_ESC, act_begin}, {S_FIN, act_mkctl}, {S_FIN, act_mkctl}, ERR,              ERR,              {S_PPH, act_setcls}, {S_PPH, act_setcls}, {S_PPH, act_setcls}, {S_CSI, act_setcls}, {S_SQC, act_setcls}, ERR,                ERR,                ERR,                },
+/* S_CSI */ {{S_ESC, act_begin}, {S_FIN, act_mkctl}, {S_CSI, act_mkpar}, ERR,              ERR,              ERR,                 ERR,                 ERR,                 ERR,                 ERR,                 {S_CSI, act_ext},   {S_CSI, act_stor},  ERR,                },
+/* S_SQC */ {{S_ESC, act_begin}, {S_SQC, act_mkstr}, {S_SQC, act_mkstr}, {S_FIN, act_nop}, {S_FIN, act_nop}, {S_SQC, act_mkstr},  {S_SQC, act_mkstr},  {S_SQC, act_mkstr},  {S_SQC, act_mkstr},  {S_SQC, act_mkstr},  {S_SQC, act_mkstr}, {S_SQC, act_mkstr}, {S_SQC, act_mkstr}, },
+/* S_PPH */ {{S_ESC, act_begin}, {S_FIN, act_mkctl}, {S_FIN, act_mkctl}, ERR,              ERR,              ERR,                 ERR,                 ERR,                 ERR,                 ERR,                 ERR,                ERR,                ERR,                },
 };
+#undef ERR
 
 int
 ansiseq_parsectl(uint8_t *data, size_t len, struct ansiseq *dst)
 {
 	size_t i = 0;
 
-	int st = ST_ST;
+	int st = S_STA;
 	struct transition tr;
 	int c;
 
@@ -173,22 +192,63 @@ ansiseq_parsectl(uint8_t *data, size_t len, struct ansiseq *dst)
 		tr.action(c);
 		st = tr.state;
 
-		if (st == ST_ERR)
+		if (st == S_ERR)
 			C("error state reached through %d", c);
 
-		if (st == ST_IGN || st == ST_FIN)
+		if (st == S_FIN)
 			break;
 	}
 
-	if (st != ST_IGN && st != ST_FIN && st != ST_ERR)
+	if (st != S_FIN)
 		return -1; //not enough data
 
-	if (st == ST_FIN)
+	if (dst)
 		*dst = s_curseq;
-	else
-		*dst = s_protoseq;
+
+	bool handled = false;
+	for(size_t j = 0; j < COUNTOF(s_cbs); j++)
+		if (s_cbs[j].fn && s_cbs[j].cmd == s_curseq.cmd
+		    && s_cbs[j].cls == s_curseq.cls)
+			s_cbs[j].fn(&s_curseq), handled = true;
+
+	if (!handled && s_cbunhandled)
+		s_cbunhandled(&s_curseq);
 
 	return i;
+}
+
+void
+ansiseq_register(uint8_t cls, uint8_t cmd, ansiseq_cb_fn fn)
+{
+	size_t i = 0;
+	while (i < COUNTOF(s_cbs) && s_cbs[i].fn)
+		i++;
+	
+	if (i == COUNTOF(s_cbs))
+		C("callback array full. bump MAX_CALLBACKS");
+
+	if (!cls && !cmd)
+		s_cbunhandled = fn;
+	else {
+		s_cbs[i].cmd = cmd;
+		s_cbs[i].cls = cls;
+		s_cbs[i].fn = fn;
+	}
+}
+
+
+void
+ansiseq_dump(struct ansiseq *cs)
+{
+	A("ansiseq class 0x%02x (%c), cmd 0x%02x (%c), ext: %d",
+	    cs->cls, cs->cls?cs->cls:'-', cs->cmd, cs->cmd?cs->cmd:'-', cs->ext);
+	A("strarg: '%s', argc: %d", cs->strarg, cs->argc);
+	for (int i = 0; i < cs->argc; i++) {
+		char x[64] = "(absent)";
+		if (cs->argv[i] != ABSENT)
+			snprintf(x, sizeof x, "%d", cs->argv[i]);
+		A("argv[%d]: '%s'", i, x);
+	}
 }
 
 /* ---------------------------------------------------------------------- */
--- src/ansiseq.h
+++ src/ansiseq.h
@@ -12,18 +12,25 @@
 
 
 #define MAX_ANSISEQ_PARAMS 8
+#define MAX_OSC_STR_SZ 256
 #define ABSENT INT_MIN
 
 
 struct ansiseq {
+	uint8_t cls;
+	uint8_t cmd;
+	uint8_t strarg[MAX_OSC_STR_SZ];
 	bool ext;
 	int argv[MAX_ANSISEQ_PARAMS];
 	int argc;
-	uint8_t cmd;
 };
 
+typedef void (*ansiseq_cb_fn)(struct ansiseq *cs);
 
 int ansiseq_parsectl(uint8_t *data, size_t len, struct ansiseq *dst);
 
+void ansiseq_register(uint8_t cls, uint8_t cmd, ansiseq_cb_fn fn);
+
+void ansiseq_dump(struct ansiseq *cs);
 
 #endif /* TELNETCAT_ANSISEQ_H */
